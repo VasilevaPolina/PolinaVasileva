486645e9a26c33a2b151f992eb9de006ee215690
Polina <vslpoulin@gmail.com>
2018-07-03 17:01:19 +0300

    homework-6 code changes

--- src/main/java/homeWorks/runners/CucumberTestngRunner.java
+++ src/main/java/homeWorks/runners/CucumberTestngRunner.java
@@ -1,25 +1,26 @@
 package homeWorks.runners;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-
-@CucumberOptions(features = "src/test/java/hw6",
+@CucumberOptions(
+        features = "src/test/java/hw6",
                 glue = "homeWorks.pageObjectsCucumber")
 public class CucumberTestngRunner extends AbstractTestNGCucumberTests {
 
-    @BeforeMethod
+   @BeforeMethod
     public void beforeMethod() {
         Configuration.browser = "chrome";
     }
 
     @AfterMethod
     public void afterTest() {
-        getWebDriver().manage().deleteAllCookies();
+        WebDriverRunner.closeWebDriver();
     }
+
 }
 
